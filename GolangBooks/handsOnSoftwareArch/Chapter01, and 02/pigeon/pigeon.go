7c29b266f6a54badc048e55d430970c6a874d1d4
Erdem Ozgen <m.erdemozgen@gmail.com>
2022-07-19 15:57:39 +0300

    update pigeon package

--- GolangBooks/handsOnSoftwareArch/Chapter01, and 02/pigeon/pigeon.go
+++ GolangBooks/handsOnSoftwareArch/Chapter01, and 02/pigeon/pigeon.go
@@ -1,9 +1,12 @@
 // Package pigeon is used to demonstrate visibily and scope for packages
 package pigeon
 
+//Name is a public attribute, and the code outside of the package can reference it.
+//featherLength is a private attribute, and the code outside of the package
+//cannot reference it.
 
 type Pigeon struct {
-	Name     string
+	Name          string
 	featherLength int
 }
 
@@ -11,7 +14,6 @@ func (p *Pigeon) GetFeatherLength() int {
 	return p.featherLength
 }
 
-
-func (p *Pigeon) SetFeatherLength(length int)  {
+func (p *Pigeon) SetFeatherLength(length int) {
 	p.featherLength = length
 }
--- GolangBooks/handsOnSoftwareArch/Chapter01, and 02/visibility.go
+++ GolangBooks/handsOnSoftwareArch/Chapter01, and 02/visibility.go
@@ -2,19 +2,18 @@
 package main
 
 import (
-	"fmt"
 	"./pigeon"
+	"fmt"
 )
-
 
 func main() {
 
 	//p := pigeon.Pigeon{"Tweety", 10} - This will not  compile
-	p := pigeon.Pigeon{Name :"Tweety", }
+	p := pigeon.Pigeon{Name: "Tweety"}
 	p.SetFeatherLength(10)
-	
+
 	fmt.Println(p.Name)
 	fmt.Println(p.GetFeatherLength())
 	//fmt.Println(p.featherLength) - This will not  compile
-	
+
 }
